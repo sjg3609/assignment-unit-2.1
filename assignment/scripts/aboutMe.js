294c7523d525da5be3ecce443b625911b7aa3702
Sam Gossie <sjg3609@gmail.com>
2023-01-16 19:49:21 -0600

    Attempted Stretch Goals

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -9,7 +9,7 @@ let fullName = firstName + (' ') + lastName;
 // 4 - Console log the value of `fullName`
 console.log(fullName);
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
-let luckyNumber = 7;
+let luckyNumber = 2;
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
@@ -58,8 +58,14 @@ if (pets < allowedPets) {
 // assigns the highest value to `mostPets`. There's several possibilities --
 // be sure to think through all the scenarios. 
 // console.log `mostPets` after the conditional has run.
-let mostPets = (pets <= friendsPets);
-console.log(mostPets);
+
+var mostPets = pets >= friendsPets;
+
+if (mostPets) {
+    console.log(mostPets);
+}
+
+
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1
 //      "Second is the best" if your lucky number is 2
@@ -67,6 +73,21 @@ console.log(mostPets);
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
 
+
+switch (luckyNumber) {
+    case 1:
+        text = "First is the worst!";
+        break;
+    case 2:
+        text = "Second is the best";
+        break;
+    case 3:
+        text =  "Third is the one with the polka dot dress";
+        break;
+    default:
+        text = "Luck is what happens when preparation meets opportnity";
+        
+} 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
 
 
